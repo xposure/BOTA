cfc01f2cd185060b4697a2c0a1d036eee684a40b
Chris <xile.xposure@gmail.com>
2015-09-23 16:46:14 -0400

    move cleanup

--- Trix/Program.cs
+++ Trix/Program.cs
@@ -5,12 +5,20 @@ namespace Trix
     /*
      * Rendering 
      *  - Chunks need to be in layers, hide no visible cubes as gray
-     *  - Culling
      *  - Cubes need to be voxelized
      *  - Should selection be a texture or another buffer? what about designations?
      *  - Floors?
      *  - Need custom shaders soon
      *  - Need font rendering for debug info
+     * Async Chunking
+     *  - Threaded chunk logic
+     * Loading
+     *  - Pulling data from HDD or generating it from noise
+     * Rebuilding
+     *  - There are different levels of rebuilding (light, mesh, water, etc?)
+     * Unloading
+     * Culling
+     *  - Breadth First Search using flood fill to determine what should be rendered
      *  
      */
 
